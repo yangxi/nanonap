79c9a9c524a3c94e8a7e81f57339291159acfc27
Xi Yang <hiyangxi@gmail.com>
2015-12-04 15:29:01 +1100

    Better output from test_latency.

--- test_latency.c
+++ test_latency.c
@@ -89,7 +89,8 @@ static hw_event_t * shim_create_hw_events(int nr_hw_events, char **hw_event_name
   return hw_events;
 }
 
-
+//return the array the kernel operats on
+//kernel sleeps on ret[0], put start tsc of "mwait" in ret[1], end of "mwait" in ret[2]
 unsigned long *kernel_tsc(unsigned long  phyaddr)
 {
   unsigned long mmap_offset = phyaddr & 0xfffffffffffff000;
@@ -118,7 +119,8 @@ static unsigned long __inline__ rdtsc(void)
 }
 
 volatile int flag = -1;
-unsigned long cycles[2];
+
+unsigned long worker_tsc[2];
 void * worker_start(void *p)
 {
   bind_processor(9);
@@ -133,16 +135,17 @@ void * worker_start(void *p)
 
   while(1) {
     if (flag == 1){
-      debug_print("start ioctl\n");
-      cycles[0] = rdtsc();
+      debug_print("Worker start ioctl\n");
+      worker_tsc[0] = rdtsc();
       ioctl(fd, 0, NULL);
-      cycles[1] = rdtsc();
+      worker_tsc[1] = rdtsc();
       flag = 2;
-      debug_print("end ioctl\n");
+      debug_print("Worker end ioctl\n");
     }
   }
 }
 
+void report_stat(int i, unsigned long *m_tsc, unsigned long *w_tsc, unsigned long *k_tsc, unsigned long *logs);
 #define LOG_SIZE 1024*1024
 //usage: test_latency phy_addr event0 event1 ...
 static char *usage = "test_latency phy_addr event0 event1 ... eventn";
@@ -153,7 +156,7 @@ int main(int ac, char **av)
     printf("%s\n", usage);
     exit(0);
   }
-  unsigned long counters[2];
+  unsigned long m_tsc[3];
   unsigned long * logs;
 
   bind_processor(1);
@@ -166,40 +169,97 @@ int main(int ac, char **av)
   unsigned long kernel_buf_addr = strtoul(av[1], NULL, 16);
   debug_print("Kernel address %lx\n", kernel_buf_addr);
   hw_event_t *events = shim_create_hw_events(ac-2, av+2);
-  unsigned long *buf = kernel_tsc(kernel_buf_addr);
+  unsigned long *k_tsc = kernel_tsc(kernel_buf_addr);
   pthread_t task;
   pthread_create(&task, NULL, worker_start, NULL);
   logs = calloc(LOG_SIZE, sizeof(unsigned long));
   memset(logs, 0, LOG_SIZE * sizeof(unsigned long));
+  //wait for worker ready
   while (flag == -1)
     ;
   //start to test
-  printf("set flag to 1\n");
-  counters[0] = rdtsc();
-  flag = 1;
-  //let's keep 1MB logs, see whether we can find interesting IPC numbers
-  //tsp_start, event0, event1, tsp_end
-  int log_index = 0;
-  while(log_index + 4 < LOG_SIZE){
-    unsigned long *p = logs + log_index;
-    p[0] = rdtsc();
-    p[1] = __builtin_ia32_rdpmc(events[0].index);
-    p[2] = __builtin_ia32_rdpmc(events[1].index);
-    p[3] = rdtsc();
-    log_index += 4;
+  //  printf("set flag to 1\n");
+  for (i=0; i<10; i=i+1) {
+    m_tsc[0] = rdtsc();
+    flag = 1;
+    //let's keep 1MB logs, see whether we can find interesting IPC numbers
+    //tsp_start, event0, event1, tsp_end
+    int log_index = 0;
+    while(log_index + 4 <= LOG_SIZE){
+      unsigned long *p = logs + log_index;
+      p[0] = rdtsc();
+      p[1] = __builtin_ia32_rdpmc(events[0].index);
+      p[2] = __builtin_ia32_rdpmc(events[1].index);
+      p[3] = rdtsc();
+      log_index += 4;
+    }
+    //  printf("touch buf[0]\n");
+    m_tsc[1] = rdtsc();
+    k_tsc[0] = 0xdead;
+    while (flag != 2)
+      ;
+    m_tsc[2] = rdtsc();
+    report_stat(i, m_tsc, worker_tsc, k_tsc, logs);
   }
-  printf("touch buf[0]\n");
-  buf[0] = 0xdead;
-  while (flag != 2)
-    ;
-  counters[1] = rdtsc();
-  printf("%lx->%lu->%lu->%lu->%lu->%lu\n",
-	 counters[0], cycles[0]-counters[0], buf[1]-counters[0], buf[2]-counters[0], cycles[1]-counters[0], counters[1]-counters[0]);
-  printf("%lu->%lu->%lu->%lu->%lu->%lu\n",
-	 counters[0], cycles[0], buf[1], buf[2], cycles[1], counters[1]);
+}
+
+
+#define PRINT_RAW_DATA
+//i->iteration, m_tsc->main_tsc, w_tsc->worker_tsc, k_tsc->kernel_tsc
+//m_tsc[0] ---- w_tsc[0] ---ioctl--k_tsc[1]---monitor/mwait----m_tsc[1]---wakeup---k_tsc[2]---ret from ioctl--w_tsc[1]--setflag---m_tsc[2]
+void report_stat(int iter, unsigned long *m_tsc, unsigned long *w_tsc, unsigned long *k_tsc, unsigned long *logs)
+{
+  int i,j,k;
+
+  printf("--------------------%d-------------\n", iter);
+  printf("#m_tsc[0] ---- w_tsc[0] ---ioctl--k_tsc[1]---monitor/mwait----m_tsc[1]---wakeup---k_tsc[2]---ret from ioctl--w_tsc[1]--setflag---m_tsc[2]\n");
+  printf("stages:%lx->%lu->%lu->%lu->%lu->%lu->%lu\n",
+	 m_tsc[0], w_tsc[0]-m_tsc[0], k_tsc[1]-w_tsc[0], m_tsc[1] - k_tsc[1],k_tsc[2] - m_tsc[1], w_tsc[1] - k_tsc[2], m_tsc[2] - w_tsc[1]);
+  //let's search the latency of releasing resources
+  unsigned long start_mwait = k_tsc[1];
+  int start_index = -1;
+  int end_index = -1;
+  for(i=0;i + 4 <=LOG_SIZE;i=i+4){
+    unsigned long *cur = logs + i;
+    unsigned long *next = cur + 4;
+    if (start_mwait >= cur[0]  &&  start_mwait <= next[0]){
+      start_index = i;
+      for(j=i;j + 4 <=LOG_SIZE;j=j+4){
+	int heat = 0;
+	int nr_step = 0;
+	for(k=j;k + 8 < LOG_SIZE;k=k+4){
+	  nr_step += 1;
+	  unsigned long *cur = logs + k;
+	  unsigned long *next = cur +4;
+	  if (next[1] - cur[1] == next[2] - cur[2])
+	    heat += 1;
+	  if (nr_step == 10 && heat == 10)
+	    end_index = j;
+	  if (nr_step == 10)
+	    break;
+	}
+	if (end_index != -1)
+	  break;
+      }
+      break;
+    }
+  }
+  printf("#start_index:%d, end_index:%d\n", start_index, end_index);
+  unsigned long mwait_latency = 0;
+  if (end_index != -1 && start_index != -1){
+    printf("#start_tsc:%lu, end_tsc:%lu\n", *(logs + start_index), *(logs+end_index));
+    mwait_latency = logs[end_index] - start_mwait;
+  }
+  printf("mwait:%lu\n", mwait_latency);
+
+#ifdef PRINT_RAW_DATA
+  int log_index;
+  printf("stages:%lu->%lu->%lu->%lu->%lu->%lu->%lu\n",
+	 m_tsc[0], w_tsc[0], k_tsc[1], m_tsc[1], k_tsc[2], w_tsc[1], m_tsc[2]);
   for (log_index=4; log_index < LOG_SIZE; log_index += 4){
     unsigned long *prev = logs + log_index - 4;
     unsigned long *curr = logs + log_index;
     printf("%lu,%lu,%lu,%lu\n", curr[0],curr[1]-prev[1],curr[2]-prev[2],curr[3]-prev[3]);
   }
+#endif
 }
